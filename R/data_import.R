edde861bda2592b835b5f02fd97bc6873acbd6fe
yusriy <yusriyp@gmail.com>
2015-09-11 16:34:12 +0800

    Added substr

--- R/data_import.R
+++ R/data_import.R
@@ -14,9 +14,10 @@
 #### 2. Import data ####
 
 # Listing all the files in the data folder
-path <- list.files('data/rawdata')
+filename <- list.files('data/rawdata')
 # To remove the '.TXT' at the end
-path1<-as.data.frame(strsplit(path,split='.TXT'))
+path <- as.data.frame(strsplit(filename,split='.TXT'))
+path <- as.character(path[,1])
 
 # Import 10 Hz data
 rawdata <- read.delim('data/rawdata/D3O_A1.TXT',sep='',header=FALSE)
@@ -25,7 +26,7 @@ rawdata <- read.delim('data/rawdata/D3O_A1.TXT',sep='',header=FALSE)
 vawt <- rep(FALSE,nrow(rawdata)) # Need to get from text file name
 
 # Creates a column for speed setting, 1 = low, 3 = high
-speed <- rep(1,nrow(rawdata)) # Need to get from the text file name
+speed <- rep(3,nrow(rawdata)) # Need to get from the text file name
 
 # Creates a column for measurement position
 pos <- rep('A1',nrow(rawdata)) # Need to get from the text file name
@@ -41,7 +42,6 @@ df <- cbind(df,rawdata)
 
 # Name the columns
 names(df) <- c('vawt','speed','dist','pos','u','v','w','T')
-
 
 # Temporary variable cleanup
 rm(pos,speed,vawt,rawdata,dist)
